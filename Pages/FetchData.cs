004f4714e8a63adff852cf0bcabc994803db7ff7
atreanor <alan.treanor@hushmail.com>
2022-02-13 19:07:27 +0000

    added operation to retrieve rest or graphql from an external endpoint

---
+++ Pages/FetchData.cs
@@ -0,0 +1,19 @@
+﻿using BlazorAPIClient.Dtos;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+
+namespace BlazorAPIClient.Pages
+{
+    public partial class FetchData
+    {   
+        [Inject]
+        private HttpClient Http { get; set; }
+
+        private LaunchDto[]? launches;
+
+        protected override async Task OnInitializedAsync()
+        {
+            launches = await Http.GetFromJsonAsync<LaunchDto[]>("/rest/launches");
+        }
+    }
+}
